db72d412f4398723b6bbfc95939452ac90f02aee
fvlaenix <fvlaenixf@gmail.com>
2024-05-20 16:55:03 +0400

    Remove useless functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,34 +1,6 @@
-import com.fvlaenix.translation.TranslationJsonService
-import java.nio.file.Path
-import kotlin.io.path.Path
-
 class Main
 
 val TOKEN =
   Main::class.java.getResourceAsStream("/token.txt")?.bufferedReader()?.readText()?.trim() ?:
   System.getenv("OPENAI_TOKEN") ?:
   throw IllegalStateException("Can't retrieve token")
-
-suspend fun gameTranslator(excelPath: String) {
-  val path = Path.of(excelPath)
-  val translationBookService = TranslationBookService(path, 0, 1, "EN", "game_id", "gpt-4")
-  try {
-    translationBookService.translate()
-  } catch (e: Exception) {
-    throw Exception(e)
-  } finally {
-    translationBookService.write(path)
-  }
-}
-
-suspend fun jsonTranslator() {
-  val path = Path("<text>")
-  val translationJsonService = TranslationJsonService(path,  "gpt-4")
-  try {
-    translationJsonService.translate()
-  } catch (e: Exception) {
-    throw Exception(e)
-  } finally {
-    translationJsonService.write(path)
-  }
-}
