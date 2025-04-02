60ddb4fc4c53452df431c1a0fe6cb2226e139dbb
fvlaenix <fvlaenixf@gmail.com>
2025-04-02 19:03:59 +0400

    Add OpenAIValidator.kt

---
+++ src/main/kotlin/com/fvlaenix/translation/validation/OpenAIValidator.kt
@@ -0,0 +1,47 @@
+package com.fvlaenix.translation.validation
+
+import TOKEN
+import com.aallam.openai.api.http.Timeout
+import com.aallam.openai.api.logging.LogLevel
+import com.aallam.openai.api.moderation.ModerationModel
+import com.aallam.openai.api.moderation.ModerationRequest
+import com.aallam.openai.api.moderation.ModerationResult
+import com.aallam.openai.client.LoggingConfig
+import com.aallam.openai.client.OpenAI
+import java.io.Closeable
+import kotlin.time.Duration.Companion.seconds
+
+class OpenAIValidator(
+  private val model: String = "omni-moderation-2024-09-26",
+) : Closeable {
+
+  val openAI = OpenAI(
+    logging = LoggingConfig(LogLevel.None),
+    token = TOKEN,
+    timeout = Timeout(socket = 120.seconds)
+  )
+
+  suspend fun makeStrictRequestFlagged(message: List<String>): Boolean {
+    val rawResult = makeRawRequest(message)
+    return rawResult.any { it.flagged }
+  }
+
+  suspend fun makeNotStrictRequestFlagged(message: List<String>): Boolean {
+    val rawResult = makeRawRequest(message)
+    return rawResult.any { it.categories.sexualMinors }
+  }
+
+  suspend fun makeRawRequest(message: List<String>): List<ModerationResult> {
+    if (message.isEmpty()) throw IllegalArgumentException("Message is empty")
+    val moderationRequest = ModerationRequest(
+      input = message,
+      model = ModerationModel(model)
+    )
+    val moderation = openAI.moderations(moderationRequest)
+    return moderation.results
+  }
+
+  override fun close() {
+    openAI.close()
+  }
+}
